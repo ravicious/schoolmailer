c5052941b533378c6e766284c67c57b3e860015e
Rafał Cieślak <ravicious@gmail.com>
2010-08-14 15:02:12 +0200

    Zabezpieczenie przed uruchomieniem testów w złym środowisku.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,9 @@ require "bundler"
 Bundler.setup(:default, :sinatra, :test)
 
 require File.join(File.dirname(__FILE__), '..', '..', 'schoolmailer')
+
+raise "Złe środowisko. Dodaj RACK_ENV=test przed poleceniem" if Schoolmailer.environment != 'test'
+
 %w(capybara capybara/cucumber spec).each {|lib| require lib}
 
 World do
